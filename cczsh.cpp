c2e6aaa32984dbf82228684c86af2fb6e4972920
Layout_404ed <layout.568.2017@gmail.com>
2025-07-02 21:13:41 -0400

    step 0, init
     did step zero [initialize the project]
    https://codingchallenges.fyi/challenges/challenge-shell/#step-zero

---
+++ cczsh.cpp
@@ -0,0 +1,19 @@
+// coding challenges shell project
+// link: https://codingchallenges.fyi/challenges/challenge-shell/#step-zero
+
+// ansi color codes
+#define RED "\\u001b[31m"
+#define RESET "\\033[0m"
+
+// libraries
+#include<iostream> // stdio
+#include<string> // strings
+
+// methods
+
+// main method
+int main(void)
+{
+    std::cout << "Project Initialized!\n";
+    return 0;
+}
